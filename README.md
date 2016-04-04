a622b4f80489c76ebf8ce060f23b8618525ac5c3
Andrew Carlson <acarl005@g.ucla.edu>
2016-04-03 22:59:26 -0700

    finish assignment 1
    remove tokens and add to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# machine-learning
+# Machine Learning
+### Coursera, taught by Andrew Ng
+
+### Class:
+[Machine Learning](https://class.coursera.org/ml "Machine Learning")
--- machine-learning-ex1/ex1/gradientDescentMulti.m
+++ machine-learning-ex1/ex1/gradientDescentMulti.m
@@ -11,7 +11,7 @@ for iter = 1:num_iters
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
-    %               theta. 
+    %               theta.
     %
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCostMulti) and gradient here.
@@ -21,15 +21,14 @@ for iter = 1:num_iters
 
 
 
-
-
-
-
+    h = X * theta;
+    delta = sum((h - y) .* X);
+    theta = theta - (alpha / m) * delta';
 
 
     % ============================================================
 
-    % Save the cost J in every iteration    
+    % Save the cost J in every iteration
     J_history(iter) = computeCostMulti(X, y, theta);
 
 end

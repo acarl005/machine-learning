295987e2f3e5e062c8f55e1a3b655f8937ccc521
Andrew Carlson <acarl005@g.ucla.edu>
2016-04-13 23:32:09 -0700

    finish implementing logistic regression

--- machine-learning-ex3/ex3/displayData.m
+++ machine-learning-ex3/ex3/displayData.m
@@ -1,12 +1,12 @@
 function [h, display_array] = displayData(X, example_width)
 %DISPLAYDATA Display 2D data in a nice grid
 %   [h, display_array] = DISPLAYDATA(X, example_width) displays 2D data
-%   stored in X in a nice grid. It returns the figure handle h and the 
+%   stored in X in a nice grid. It returns the figure handle h and the
 %   displayed array if requested.
 
 % Set example_width automatically if not passed in
-if ~exist('example_width', 'var') || isempty(example_width) 
-	example_width = round(sqrt(size(X, 2)));
+if ~exist('example_width', 'var') || isempty(example_width)
+	example_width = round(sqrt(size(X, 2))); % 20
 end
 
 % Gray Image
@@ -14,11 +14,11 @@ colormap(gray);
 
 % Compute rows, cols
 [m n] = size(X);
-example_height = (n / example_width);
+example_height = (n / example_width); % 20
 
 % Compute number of items to display
-display_rows = floor(sqrt(m));
-display_cols = ceil(m / display_rows);
+display_rows = floor(sqrt(m)); % 10
+display_cols = ceil(m / display_rows); % 10
 
 % Between images padding
 pad = 1;
@@ -31,11 +31,11 @@ display_array = - ones(pad + display_rows * (example_height + pad), ...
 curr_ex = 1;
 for j = 1:display_rows
 	for i = 1:display_cols
-		if curr_ex > m, 
-			break; 
+		if curr_ex > m,
+			break;
 		end
 		% Copy the patch
-		
+
 		% Get the max value of the patch
 		max_val = max(abs(X(curr_ex, :)));
 		display_array(pad + (j - 1) * (example_height + pad) + (1:example_height), ...
@@ -43,8 +43,8 @@ for j = 1:display_rows
 						reshape(X(curr_ex, :), example_height, example_width) / max_val;
 		curr_ex = curr_ex + 1;
 	end
-	if curr_ex > m, 
-		break; 
+	if curr_ex > m,
+		break;
 	end
 end
 
